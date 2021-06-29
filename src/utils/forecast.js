84458c7cbcd8270dc52a8f4192b021081a92d548
marygladyfaustino <marygladyfaustino@gmail.com>
2021-06-30 05:25:31 +0800

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const forecast = (longlitude, langtitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=ce8a75dbdce2a5cd59cab3165ff270e4&query=${langtitude},${longlitude}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degrees out.It feels like ${body.current.feelslike} degrees out`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
